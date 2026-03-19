965554ecdfd21c059e9b7ae79d062bafefbb43fc
James Thompson <jamesgct96@gmail.com>
2026-03-18 21:47:29 -0400

    vault

---
+++ index.js
@@ -0,0 +1,8 @@
+let code1;
+code1 = 7 + 3;
+let code2 = 90 / 2 - 5;
+let code3 = (2 % 2) + 39;
+let message = "The vault has been secured. The combination is:";
+let codeA = code1 + "-" + code2 + "-" + code3;
+let codeB = `${code1} - ${code2} - ${code3}`;
+console.log(message, codeA, codeB);
